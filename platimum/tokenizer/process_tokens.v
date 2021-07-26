7d4b9d9a64a2c54dabc66906e3a36f8e87968353
Saptak <saptakbhoumik@gmail.com>
2021-07-26 16:14:31 +0530

    happy to some extent

--- platimum/tokenizer/process_tokens.v
+++ platimum/tokenizer/process_tokens.v
@@ -3,18 +3,16 @@ module tokenizer
 pub fn remove_white_space(list []string) []string{
 	mut lis := list.clone()
 	//defining an empty array
-	mut results := []string{}
+	mut results := [" "]
 	for i,mut item in lis{
 		mut count:=results.len
 		if i>0{
 			mut prev_item:= results[count-1]
 			if count>0{
 				if prev_item !=" " && item!=" "{
-					mut append_index:="$prev_item$item"
-					print(append_index)
-					// unsafe{
-					//  results[count-1]+=*item
-					// }
+					unsafe{
+					 results[count-1]+=*item
+					}
 				}
 				else if prev_item !=" " && item==" "{
 					results << item
