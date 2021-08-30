49b0cbd4ad678cf0c200d074cb45e5db5843584a
mdgaziur <mdgaziurrahmannoor@gmail.com>
2021-08-30 16:28:31 +0600

    Builtin: Add _format() function for string formatting

---
+++ tests/builtin/format.c
@@ -0,0 +1,13 @@
+// Original author: MD Gaziur Rahman Noor
+/* Tests for _format() builtin function */
+#include<assert.h>
+#include "../../swallow/builtin.h"
+
+int main() {
+    assert(strcmp(_format("Hello {}!", "world"), "Hello world!") == 0);
+    assert(strcmp(_format(""), "") == 0);
+    assert(strcmp(_format("{", "someRandomString"), "{") == 0);
+    assert(strcmp(_format("Hello"), "Hello") == 0);
+    assert(strcmp(_format("{}, welcome!", "someRandomPerson\0"), "someRandomPerson, welcome!") == 0);
+    assert(strcmp(_format("{}", "format1"), "format1") == 0);
+}
