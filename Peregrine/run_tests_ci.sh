701cfbae86d88359d91bca5cd5d4452a7dfa1b8f
ClientCrash <clientcrashyt@gmail.com>
2021-10-07 21:35:05 +0200

    fixed bug

--- Peregrine/run_tests_ci.sh
+++ Peregrine/run_tests_ci.sh
@@ -3,13 +3,14 @@ cd ./tests
 pwd
 for file in *
 do
-    echo $file ...
-    ../peregrine compile $file -emit-c
-    if gcc temp.c -o output.o then
-        echo "::error GCC FAILED"
-    else
-        chmod +x ./output.o
-        ./output.o
-        rm ./output.o
+echo $file ...
+../peregrine compile $file -emit-c
+if gcc temp.c -o output.o then
+echo "::error GCC FAILED"
+else
+chmod +x ./output.o
+./output.o
+rm ./output.o
+fi
     
 done
