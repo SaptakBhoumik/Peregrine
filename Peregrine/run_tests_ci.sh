e09cfca5dda6c5b1c6127deeeb97cbe7f97e1979
ClientCrash <clientcrashyt@gmail.com>
2021-10-07 21:24:56 +0200

    fixed typo

--- Peregrine/run_tests_ci.sh
+++ Peregrine/run_tests_ci.sh
@@ -3,9 +3,11 @@ cd ./tests
 pwd
 for file in *
 do
+    echo $file ...
     ../peregrine compile $file -emit-c
     gcc temp.c -o output.o
     chmod +x ./output.o
     ./output.o
     rm ./output.o
+    
 done
