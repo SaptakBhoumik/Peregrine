e05ea7eef2cdf32acdd47ba0fae7edb4ab5a190f
Pavan D Umesh <85598484+Pavan2027@users.noreply.github.com>
2021-10-16 13:39:31 +0530

    Updated Tokenizer
    Updated the keywords and operators of the tokenizer by using reference from Python 3.10.0

--- Peregrine/tokenizer/tokenizer.v
+++ Peregrine/tokenizer/tokenizer.v
@@ -2,128 +2,179 @@ module tokenizer
 
 // Original author: Saptak Bhoumik
 pub fn tokenize(str string) []string {
-	// it partially tokenizes the code
-	other_keywords := [
+	arithmetic_operators := [
+		'+',
+		'-',
+		'*',
+		'/',
+		'%',
+		'**',
+		'//',
+	]
+	
+	assignment_operators := [
+		'=',
+		'+=',
+		'-=',
+		'*=',
+		'/=',
+		'%=',
+		'//=',
+		'**=',
+		'&=',
+		'|=',
+		'^=',
+		'>>=',
+		'<<=',
+	]
+	
+	comparison_operators := [
+		'==',
+		'!=',
+		'>',
+		'<',
+		'>=',
+		'<=',
+		'is',
+		'in',
+	]
+		
+	logical_operators := [
+		'and',
+		'or',
+		'not',
+	]
+	
+	identity_operators := [
+		'is',
+		'is not',
+	]
+	
+	membership_operators := [
+		'in',
+		'not in',
+	]
+	
+	bitwise_operators := [
+		'&',
+		'|',
+		'^',
+		'~',
+		'<<',
+		'>>',
+	]
+	
+	value_keywords := [
 		'True',
 		'False',
 		'None',
 	]
-	symbols := [
-		' ',
-		// white space
-		'!',
-		'{',
-		'}',
-		'(',
-		')',
-		'[',
-		']',
-		'.',
-		'"',
-		"'",
-		'\n',
-		':',
-		',',
-		'#',
-		// comment
-		'`',
-		// acute sign
-	]
-	logical_keywords := [
-		'&',
-		'|',
-		'~',
-		'=',
-		// equals
-		'+',
-		// addition
-		'-',
-		// subtraction
-		'*',
-		// multiplication
-		'/',
-		// division
-		'^',
-		// exponent
-		'//',
-		// quotient
-		'%',
-		// remainder
-		'>',
-		// greater than
-		'<',
-		// less than
-		'>=',
-		// greater than equals to
-		'<=',
-		// less than equals to
-		'!=',
-		// not equals to
-	]
-
-	operator_keywords := [
-		'and',
-		'or',
-		'not',
-		'in',
-		'is',
-	]
+	
 	conditional_keywords := [
 		'if',
 		'elif',
 		'else',
 	]
+	
 	iteration_keywords := [
 		'for',
 		'while',
 		'break',
 		'continue',
-		'pass',
 	]
+	
 	structure_keywords := [
 		'def',
+		'class',
+		'with',
+		'as',
+		'pass',
+		'lambda',
 	]
+	
 	return_keywords := [
 		'return',
+		'yield',
 	]
+	
 	import_keywords := [
 		'import',
 		'himport',
 		'cimport',
+		'from',
+		'as',
 	]
+	
 	exception_keywords := [
 		'try',
 		'except',
 		'raise',
 		'finally',
+		'assert',
+	]
+	
+	asynchronous_keywords := [
+		'async',
+		'await',
 	]
 
-	variable_keybords := [
+	variable_handling_keywords := [
+		'del',
+		'global',
+		'nonlocal',
+	]
+	
+	datatypes := [
+		// Binary Types
+		'memoryview', 
+		'bytearray', 
+		'bytes'.
+		
+		// Numeric Types
+		'complex', 
+		'float', 
 		'int',
-		'bool',
+		
+		// Text Type
 		'str',
+		
+		// Boolean Type
+		'bool',
+		
+		// Set Types
+		'frozenset',
+		'set',
+		
+		// Mapping Type
+		'dict',
+		
+		// Sequence Types
+		'range', 
+		'tuple', 
 		'list',
-		'dictionary',
-		'float',
-		'void',
-		'const',
 	]
 
 	// defining an empty array
 	mut collection_of_keywords := []string{}
 
 	// appending the keywords
-	collection_of_keywords << other_keywords
-	collection_of_keywords << symbols
-	collection_of_keywords << logical_keywords
-	collection_of_keywords << operator_keywords
+	collection_of_keywords << arithmetic_operators
+	collection_of_keywords << assignment_operators
+	collection_of_keywords << comparison_operators
+	collection_of_keywords << logical_operators
+	collection_of_keywords << identity_operators
+	collection_of_keywords << membership_operators
+	collection_of_keywords << bitwise_operators
+	collection_of_keywords << value_keywords
 	collection_of_keywords << conditional_keywords
 	collection_of_keywords << iteration_keywords
 	collection_of_keywords << structure_keywords
-	collection_of_keywords << return_keywords
+	collection_of_keywords << return_keyword
 	collection_of_keywords << import_keywords
 	collection_of_keywords << exception_keywords
-	collection_of_keywords << variable_keybords
+	collection_of_keywords << asynchronous_keywords
+	collection_of_keywords << variable_handling_keywords
+	collection_of_keywords << datatypes
 	mut code := '\n$str  ' // do not change this line
 	white_space := ' '
 
